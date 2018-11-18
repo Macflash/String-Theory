0f5a9bd78e523f6a365fc160104e409a0d797090
Alex Breyfogle <alexbr@microsoft.com>
2018-11-18 15:35:22 -0800

    connect simple redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import { ScaleComponent } from './core/music/scale';
+import { AppStore } from './redux/reducers';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            string-theory
-          </p>
-          <ScaleComponent />
-        </header>
-      </div>
+      <Provider store={AppStore}>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              string-theory
+            </p>
+            <ScaleComponent />
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,6 +1,19 @@
+import {combineReducers, AnyAction, createStore} from 'redux';
+
 export type INoteLookup = {[note: number]: boolean | undefined };
 
-export interface IAppState {
+export interface IStringTheoryState {
     selectedNotes: INoteLookup;
-    hoveredNotes: INoteLookup;
+    //hoveredNotes: INoteLookup;
 }
+
+
+function selectedNotes(state: INoteLookup | undefined, action: AnyAction): INoteLookup {
+    return {};
+}
+
+const appReducers = combineReducers<IStringTheoryState>({
+    selectedNotes,
+});
+
+export const AppStore = createStore(appReducers);
